6fb0692398b7e1c6c11ce8094112d21868854500
SunglassmanAlex <184515571+SunglassmanAlex@users.noreply.github.com>
2026-03-19 20:43:38 +0800

    feat: finish is_prime func

--- is_prime/is_prime.go
+++ is_prime/is_prime.go
@@ -1,8 +1,22 @@
 package is_prime
 
-import "math/big"
+import (
+	"math/big"
+)
 
 func IsPrime(n *big.Int) bool {
-	// TODO: write a method to determine if n is a prime
-	return false
+	// write a method to determine if n is a prime
+	bigOne := big.NewInt(1)
+	bigZero := big.NewInt(0)
+	for i := big.NewInt(2); i.Cmp(n) <= 0; i.Add(i, bigOne) {
+		r := new(big.Int).Mod(n, i)
+		tmp := new(big.Int).Mul(i, i)
+		if tmp.Cmp(n) > 0 {
+			break
+		}
+		if r.Cmp(bigZero) == 0 {
+			return false
+		}
+	}
+	return true
 }
--- is_prime/is_prime_test.go
+++ is_prime/is_prime_test.go
@@ -1,1 +1,21 @@
 package is_prime
+
+import (
+	"crypto/rand"
+	"math/big"
+	"testing"
+)
+
+func TestIsPrime(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		n, err := rand.Int(rand.Reader, big.NewInt(100000000))
+		if err != nil {
+			panic("Oh my god!")
+		}
+		expected := n.ProbablyPrime(20)
+		got := IsPrime(n)
+		if expected != got {
+			t.Errorf("恢复失败：n = %s, expected %v, got %v", n.String(), expected, got)
+		}
+	}
+}
